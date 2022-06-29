c5d2ca7a0b61f31cc772b70a3d5039d210b59612
Knight174 <cpc4253@163.com>
2022-06-29 14:23:05 +0800

    chore(dev_webpack): add proxy in devServer config

--- build/webpack.dev.config.ts
+++ build/webpack.dev.config.ts
@@ -13,6 +13,15 @@ const devConfig = {
     compress: true, // 开启 gzip 压缩
     port: 9000, // 自定义端口号
     hot: 'only', // 开启热更新
+    proxy: {
+      '/api': {
+        target: 'https://res.abeim.cn', // 匹配到 '/api' 时，代理为 'https://res.abeim.cn/api'
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '' // 将 'https://res.abeim.cn/api' 中的 '/api' 替换为空
+        }
+      }
+    }
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
