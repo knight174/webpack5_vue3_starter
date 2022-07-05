7c360ce6d96e168b6942d7af1d64634af0de8f63
Knight174 <cpc4253@163.com>
2022-07-05 15:47:36 +0800

    chore: add husky and lint-staged

--- src/views/Vuex.vue
+++ src/views/Vuex.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useStore } from 'vuex'
+
 const store = useStore()
 const firstName = ref('')
 
@@ -14,12 +15,7 @@ function updateName() {
   <h1>Hello, Vuex!</h1>
   <label for="name-input" class="form-group">
     <h1>Update First Name:</h1>
-    <input
-      type="text"
-      id="name-input"
-      :placeholder="firstName ? '' : 'Please input your name.'"
-      v-model="firstName"
-    />
+    <input type="text" id="name-input" :placeholder="firstName ? '' : 'Please input your name.'" v-model="firstName" />
   </label>
   <el-button @click="updateName">Update User Name</el-button>
 </template>
@@ -29,6 +25,7 @@ function updateName() {
   display: block;
   width: 50%;
   margin: 0 auto;
+
   h1 {
     display: inline;
     margin-right: 10px;
