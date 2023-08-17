7452d89b0947969ffb77691c654d8df8a0fe2ac1
pinchao <cpc4253@163.com>
2023-08-17 21:44:21 +0800

    feat(ts config): add baseUrl and paths
    避免ts文件中使用 @ 别名时报错

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import './style/reset.css'
-import router from './router/index'
+import router from '@/router/index'
 import store from './store/index'
 
 createApp(App).use(router).use(store).mount('#app')
