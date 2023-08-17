87255fed9b358e907220533d1f29d051aea3741c
pinchao <cpc4253@163.com>
2023-08-17 21:34:31 +0800

    feat(webpack configs): 打包配置调整

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -15,7 +15,29 @@ const prodConfig = {
     maxAssetSize: 30000000
   },
   optimization: {
-    usedExports: true // 消除未使用的导出（exports）代码
+    usedExports: true, // 消除未使用的导出（exports）代码
+    // 代码分割
+    splitChunks: {
+      chunks: 'all', // 同步或异步
+      minSize: 0, // 最小分割大小
+      // 缓存分割大小
+      cacheGroups: {
+        // 打包 node_modules 中引用的第三方库的模块（优先级更高）
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          enforce: true, // 最高级别缓存（无视其他缓存组的优先级）
+          priority: -10,
+          name: 'vendors'
+        },
+        // 公共模块
+        common: {
+          minChunks: 2, // 至少在两个模块中引用的模块（将被分离到 common 代码中）
+          priority: -20,
+          reuseExistingChunk: true, // 模块缓存重用
+          name: 'common'
+        }
+      }
+    }
   }
 }
 
